27887fbb8d8d038465845d3dcacac7a970b656fc
jdooley <60899694+jonathan-dooley24@users.noreply.github.com>
2020-03-11 00:18:46 -0500

    Update transforms.js

--- scripts/transforms.js
+++ scripts/transforms.js
@@ -1,9 +1,9 @@
 // set values of mat4x4 to the identity matrix
 function Mat4x4Identity(mat4x4) {
 	mat4x4.values = [
-					1,0,0,0 //index 0 - 3
-					0,1,0,0 //index 4 - 7
-					0,0,1,0 //index 8 - 11
+					1,0,0,0, //index 0 - 3
+					0,1,0,0, //index 4 - 7
+					0,0,1,0, //index 8 - 11
 					0,0,0,1 //index 12 - 15
 					]
 }
@@ -14,7 +14,7 @@ function Mat4x4Translate(mat4x4, tx, ty, tz) {
 	mat4x4.values = [
 					1,0,0,tx, //index 0 - 3
 					0,1,0,ty, //index 4 - 7
-					0,0,1,tz //index 8 - 11
+					0,0,1,tz, //index 8 - 11
 					0,0,0,1 //index 12 - 15
 					]
 }
@@ -25,7 +25,7 @@ function Mat4x4Scale(mat4x4, sx, sy, sz) {
 	mat4x4.values = [
 					sx,0,0,0, //index 0 - 3
 					0,sy,0,0, //index 4 - 7
-					0,0,sz,0 //index 8 - 11
+					0,0,sz,0, //index 8 - 11
 					0,0,0,1 //index 12 - 15
 					]
 }
@@ -36,7 +36,7 @@ function Mat4x4RotateX(mat4x4, theta) {
 	mat4x4.values = [
 					1,0,0,0, //index 0 - 3
 					0,Math.cos(theta),(Math.sin(theta) * -1),0, //index 4 - 7
-					0,Math.sin(theta),Math.cos(theta),0 //index 8 - 11
+					0,Math.sin(theta),Math.cos(theta),0, //index 8 - 11
 					0,0,0,1 //index 12 - 15
 					]
 }
@@ -47,7 +47,7 @@ function Mat4x4RotateY(mat4x4, theta) {
 	mat4x4.values = [
 					Math.cos(theta),0,Math.sin(theta),0, //index 0 - 3
 					0,1,0,0, //index 4 - 7
-					(Math.sin(theta) * -1),0,Math.cos(theta),0 //index 8 - 11
+					(Math.sin(theta) * -1),0,Math.cos(theta),0, //index 8 - 11
 					0,0,0,1 //index 12 - 15
 					]
 }
@@ -58,7 +58,7 @@ function Mat4x4RotateZ(mat4x4, theta) {
 	mat4x4.values = [
 					Math.cos(theta),(Math.sin(theta) * -1),0,0, //index 0 - 3
 					Math.sin(theta),Math.cos(theta),0,0, //index 4 - 7
-					0,0,1,0 //index 8 - 11
+					0,0,1,0, //index 8 - 11
 					0,0,0,1 //index 12 - 15
 					]
 }
@@ -69,7 +69,7 @@ function Mat4x4ShearXY(mat4x4, shx, shy) {
 	mat4x4.values = [
 					1,0,shx,0, //index 0 - 3
 					0,1,shy,0, //index 4 - 7
-					0,0,1,0 //index 8 - 11
+					0,0,1,0, //index 8 - 11
 					0,0,0,1 //index 12 - 15
 					]
 }
