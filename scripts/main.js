a15fed4487d9a081c7b5cf17bf805e3c407d8c9b
jdooley <60899694+jonathan-dooley24@users.noreply.github.com>
2020-03-11 13:19:35 -0500

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -16,9 +16,11 @@ function CalculateCompoundTransform(transforms) {
 		compound_transform = transforms[0].mat4x4;
 		for(var i = 1; i < transforms.length; i++){
 			compound_transform = Matrix.multiply([transforms[i].mat4x4, compound_transform]) //IS THIS THE RIGHT ORDER OF MULT?
-			console.log("#" + i + " loop\n" + "current_transforms: " + transforms[i].mat4x4 + "\ncompound: " + compound_transform);
+			
 		}
 	}
+	
+	
 	
     return compound_transform;
 }
